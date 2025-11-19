52668da6c6fa849d586f9565972d6bba627519f3
Emmanuel <ep9186885@gmail.com>
2025-11-19 13:07:09 -0800

    Commit Pyhon Projects

---
+++ python Folder/index.py
@@ -0,0 +1,107 @@
+# # name = "Emmanuel"
+# # print(name)
+
+# # decimal = 3.14
+# # print(decimal)
+
+# # boolean = True
+# # print(boolean)
+
+# # interger = 42
+# # print("integer")
+
+# # num1= 100
+# # num2 = 50
+# # sum = num1 + num2
+# # print(sum)
+
+# # num1= 100
+# # num2 = 50
+# # sub = num1 - num2
+# # print(sub)
+
+# # num1= 100
+# # num2 = 50
+# # sum = num1 > num2
+# # print(sum)
+
+# # student_name = ["jefferey", "Micheal","Ann"]
+# # print("students_name"[0])
+
+# # courses = ("Math", "Science","History")
+# # print(courses)
+
+# # students_lastname = {"jefferey", "Michael", "johnson"}
+# # print(courses)
+
+# # car = {
+# #     "color": "Red",
+# #     "Model": "Toyota",
+# #     "Year": 2020
+# # }
+# # print(["Model"])
+
+# # age = 18
+# # age = int(input("Enter your age: "))
+
+# # if age >= 18:
+# #     print("You are eligible to vote")
+# # else:
+# #     print("You are not eligible to vote")
+
+
+# # score = 75
+
+# # if score >= 80:
+# #     print("Grade: A")
+# # elif score >= 60:
+# #     print("Grade: B")
+# # else:
+# #     print("Grade: C")
+
+# # count = 0
+# # while count < 5:
+# #     print("count is", count)
+# #     count += 1
+
+# correct_password = "backend123"
+# attempts = 0
+
+# while attempts <5:
+#     password = input("Enter your password")
+#     print("login successful")
+#     break
+# else:
+#     print("wrong password")
+# attempts += 1
+
+# if attempts ==5:
+#     print("Account blocked: Too many attempt")
+
+# def send_mail():
+#     print("Welcome Charles, We will be having tomorrow")
+
+
+#     send_mail()
+
+
+#     def send_message():
+#         print("Hello, Welcome to your dashboard.")
+
+#         send_message("Micheal")
+#         send_message("sarah")
+#         send_message("David")
+#         send_message("Anna")
+        
+
+
+
+
+def send_welcome_mail(user):
+
+  print("Welcome,", user + "Welcome to your dashboard.")
+        
+send_welcome_mail("convenant")
+send_welcome_mail("Ann")
+send_welcome_mail("john")
+
